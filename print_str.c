9f6a02892c2a1239499e0ebfa08481f0125291d9
AzerothA <telifesite@gmail.com>
2023-07-25 13:58:32 +0330

    print_str file added

---
+++ print_str.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+// Prints all of the string (chars)
+// expects pointer to the string
+int print_str(char *str) {
+  // The pointer is null :|
+  // What am i goin to do with null pointer ?
+  if (str == NULL)
+    return 1;
+
+  // create a new pointer, a copy from the original pointer
+  char *pointer = str;
+
+  do {
+    // Print the current char (derefrence it)
+    fprintf(stdout, "%c", *pointer);
+
+    // Increment pointer (next char)
+    pointer++;
+  }
+
+  // if the current char is not equal to 0
+  // continue
+  //
+  // When we create a string in c, it will allocate the size of
+  // the string + 1, and it will put 0 at the end of the string
+  // so the developers can determine the end of the string
+  while (*(pointer) != 0);
+
+  return 0;
+}
+
+int main(void) {
+  char *some_string = "Hello World";
+  int ret = print_str(some_string);
+
+  printf(" %i \n", ret);
+  return 0;
+}
