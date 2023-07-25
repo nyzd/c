b788acfd0b9fed02c751f6021e3262b51ee3ab15
AzerothA <telifesite@gmail.com>
2023-07-25 15:18:47 +0330

    Dynamic array added

---
+++ dynamic_array.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define Uint unsigned int
+
+typedef struct array_type {
+  Uint capacity;
+  Uint length;
+
+  Uint *array;
+} Array;
+
+// Creates the new array and returns the pointer
+Array *new_array() {
+  // Allocate the memory
+  Array *new_a = malloc(sizeof(Array));
+
+  new_a->capacity = 1;
+  new_a->length = 0;
+
+  // Allocate array for just one item
+  new_a->array = malloc(1);
+
+  return new_a;
+}
+
+// This is almost O(1)
+int array_push(Array *array, int item) {
+  if (array->capacity == array->length) {
+    // We need to reallocate
+    Uint *new_alloc = realloc(array->array, array->capacity * 2);
+
+    if (new_alloc == NULL) {
+      return 1;
+    }
+
+    array->capacity *= 2;
+
+    // New pointer
+    array->array = new_alloc;
+  }
+
+  // Now we insert
+  // We need to insert to the
+  array->array[array->length] = item;
+
+  array->length += 1;
+
+  return 0;
+}
+
+Uint *array_get(Array *array, Uint index) {
+  if (index >= array->length) {
+    // Index out of bound
+    printf("Index out of bound %i", index);
+    exit(1);
+  }
+
+  return &array->array[index];
+}
+
+Array *array_copy(Array *array) {
+  Array *clone_array = new_array();
+  Uint i = 0;
+
+  do {
+    array_push(clone_array, *array_get(array, i));
+
+    i += 1;
+  } while (array->length != i);
+
+  return clone_array;
+}
+
+int main(void) {
+  Array *a1 = new_array();
+
+  array_push(a1, 10);
+  array_push(a1, 256);
+  array_push(a1, 'c');
+
+  Array *a1_clone = array_copy(a1);
+  Uint *first_item = array_get(a1_clone, 1);
+
+  printf("%i %i, %i, a1: %p a1_clone: %p", a1->length, a1->capacity,
+         *first_item, a1, a1_clone);
+
+  return 0;
+}
